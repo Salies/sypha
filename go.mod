59a630c42d4a9d20aa3fc3cdf63c2b8efd0e2a50
Daniel Serezane <serezane@protonmail.com>
2023-11-11 20:28:24 -0300

    feat: tidy apu

--- go.mod
+++ go.mod
@@ -5,6 +5,5 @@ go 1.21.3
 require (
 	github.com/go-gl/gl v0.0.0-20190320180904-bf2b1f2f34d7
 	github.com/gordonklaus/portaudio v0.0.0-20180817120803-00e7307ccd93
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20231102141658-eca20e8abded
 )
-
-require github.com/go-gl/glfw/v3.3/glfw v0.0.0-20231102141658-eca20e8abded
---
+++ nes/dmc.go
@@ -0,0 +1,92 @@
+package nes
+
+type DMC struct {
+	cpu            *CPU
+	enabled        bool
+	value          byte
+	sampleAddress  uint16
+	sampleLength   uint16
+	currentAddress uint16
+	currentLength  uint16
+	shiftRegister  byte
+	bitCount       byte
+	tickPeriod     byte
+	tickValue      byte
+	loop           bool
+	irq            bool
+}
+
+func (d *DMC) writeControl(value byte) {
+	d.irq = value&0x80 == 0x80
+	d.loop = value&0x40 == 0x40
+	d.tickPeriod = dmcTable[value&0x0F]
+}
+
+func (d *DMC) writeValue(value byte) {
+	d.value = value & 0x7F
+}
+
+func (d *DMC) writeAddress(value byte) {
+	// Sample address = %11AAAAAA.AA000000
+	d.sampleAddress = 0xC000 | (uint16(value) << 6)
+}
+
+func (d *DMC) writeLength(value byte) {
+	// Sample length = %0000LLLL.LLLL0001
+	d.sampleLength = (uint16(value) << 4) | 1
+}
+
+func (d *DMC) restart() {
+	d.currentAddress = d.sampleAddress
+	d.currentLength = d.sampleLength
+}
+
+func (d *DMC) stepTimer() {
+	if !d.enabled {
+		return
+	}
+	d.stepReader()
+	if d.tickValue == 0 {
+		d.tickValue = d.tickPeriod
+		d.stepShifter()
+	} else {
+		d.tickValue--
+	}
+}
+
+func (d *DMC) stepReader() {
+	if d.currentLength > 0 && d.bitCount == 0 {
+		d.cpu.stall += 4
+		d.shiftRegister = d.cpu.Read(d.currentAddress)
+		d.bitCount = 8
+		d.currentAddress++
+		if d.currentAddress == 0 {
+			d.currentAddress = 0x8000
+		}
+		d.currentLength--
+		if d.currentLength == 0 && d.loop {
+			d.restart()
+		}
+	}
+}
+
+func (d *DMC) stepShifter() {
+	if d.bitCount == 0 {
+		return
+	}
+	if d.shiftRegister&1 == 1 {
+		if d.value <= 125 {
+			d.value += 2
+		}
+	} else {
+		if d.value >= 2 {
+			d.value -= 2
+		}
+	}
+	d.shiftRegister >>= 1
+	d.bitCount--
+}
+
+func (d *DMC) output() byte {
+	return d.value
+}
