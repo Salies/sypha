9526c7e94c3cfe390f3b2de6d4fdb0028fc47e80
Salies <serezane@protonmail.com>
2023-11-11 03:07:58 -0300

    cleanup

--- ui/util.go
+++ ui/util.go
@@ -1,56 +1,12 @@
 package ui
 
 import (
-	"crypto/md5"
-	"encoding/binary"
-	"fmt"
 	"image"
-	"image/color"
-	"image/draw"
-	"image/gif"
-	"image/png"
-	"io/ioutil"
-	"log"
-	"os"
-	"os/user"
-	"path"
 
 	"github.com/fogleman/nes/nes"
 	"github.com/go-gl/gl/v2.1/gl"
 	"github.com/go-gl/glfw/v3.2/glfw"
 )
-
-var homeDir string
-
-func init() {
-	u, err := user.Current()
-	if err != nil {
-		log.Fatalln(err)
-	}
-	homeDir = u.HomeDir
-}
-
-func thumbnailURL(hash string) string {
-	return "http://www.michaelfogleman.com/static/nes/" + hash + ".png"
-}
-
-func thumbnailPath(hash string) string {
-	return homeDir + "/.nes/thumbnail/" + hash + ".png"
-}
-
-func sramPath(hash string, snapshot int) string {
-	if snapshot >= 0 {
-		return fmt.Sprintf("%s/.nes/sram/%s-%d.dat", homeDir, hash, snapshot)
-	}
-	return fmt.Sprintf("%s/.nes/sram/%s.dat", homeDir, hash)
-}
-
-func savePath(hash string, snapshot int) string {
-	if snapshot >= 0 {
-		return fmt.Sprintf("%s/.nes/save/%s-%d.dat", homeDir, hash, snapshot)
-	}
-	return fmt.Sprintf("%s/.nes/save/%s.dat", homeDir, hash)
-}
 
 func readKey(window *glfw.Window, key glfw.Key) bool {
 	return window.GetKey(key) == glfw.Press
@@ -102,31 +58,12 @@ func readJoystick(joy glfw.Joystick, turbo bool) [8]bool {
 	return result
 }
 
-func joystickReset(joy glfw.Joystick) bool {
-	if !glfw.JoystickPresent(joy) {
-		return false
-	}
-	buttons := glfw.GetJoystickButtons(joy)
-	if len(buttons) < 6 {
-		return false
-	}
-	return buttons[4] == 1 && buttons[5] == 1
-}
-
 func combineButtons(a, b [8]bool) [8]bool {
 	var result [8]bool
 	for i := 0; i < 8; i++ {
 		result[i] = a[i] || b[i]
 	}
 	return result
-}
-
-func hashFile(path string) (string, error) {
-	data, err := ioutil.ReadFile(path)
-	if err != nil {
-		return "", err
-	}
-	return fmt.Sprintf("%x", md5.Sum(data)), nil
 }
 
 func createTexture() uint32 {
@@ -146,97 +83,4 @@ func setTexture(im *image.RGBA) {
 	gl.TexImage2D(
 		gl.TEXTURE_2D, 0, gl.RGBA, int32(size.X), int32(size.Y),
 		0, gl.RGBA, gl.UNSIGNED_BYTE, gl.Ptr(im.Pix))
-}
-
-func copyImage(src image.Image) *image.RGBA {
-	dst := image.NewRGBA(src.Bounds())
-	draw.Draw(dst, dst.Rect, src, image.ZP, draw.Src)
-	return dst
-}
-
-func loadPNG(path string) (image.Image, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-	return png.Decode(file)
-}
-
-func savePNG(path string, im image.Image) error {
-	file, err := os.Create(path)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-	return png.Encode(file, im)
-}
-
-func saveGIF(path string, frames []image.Image) error {
-	var palette []color.Color
-	for _, c := range nes.Palette {
-		palette = append(palette, c)
-	}
-	g := gif.GIF{}
-	for i, src := range frames {
-		if i%3 != 0 {
-			continue
-		}
-		dst := image.NewPaletted(src.Bounds(), palette)
-		draw.Draw(dst, dst.Rect, src, image.ZP, draw.Src)
-		g.Image = append(g.Image, dst)
-		g.Delay = append(g.Delay, 5)
-	}
-	file, err := os.Create(path)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-	return gif.EncodeAll(file, &g)
-}
-
-func screenshot(im image.Image) {
-	for i := 0; i < 1000; i++ {
-		path := fmt.Sprintf("%03d.png", i)
-		if _, err := os.Stat(path); os.IsNotExist(err) {
-			savePNG(path, im)
-			return
-		}
-	}
-}
-
-func animation(frames []image.Image) {
-	for i := 0; i < 1000; i++ {
-		path := fmt.Sprintf("%03d.gif", i)
-		if _, err := os.Stat(path); os.IsNotExist(err) {
-			saveGIF(path, frames)
-			return
-		}
-	}
-}
-
-func writeSRAM(filename string, sram []byte) error {
-	dir, _ := path.Split(filename)
-	if err := os.MkdirAll(dir, 0755); err != nil {
-		return err
-	}
-	file, err := os.Create(filename)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-	return binary.Write(file, binary.LittleEndian, sram)
-}
-
-func readSRAM(filename string) ([]byte, error) {
-	file, err := os.Open(filename)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-	sram := make([]byte, 0x2000)
-	if err := binary.Read(file, binary.LittleEndian, sram); err != nil {
-		return nil, err
-	}
-	return sram, nil
 }
